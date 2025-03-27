6b935d37451cd17542f227bc14647eac36090813
Pinku Rathour <pinkurathour2016@gmail.com>
2025-03-27 15:12:22 +0530

    fix text size

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Todos from "./Components/Todos";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-medium text-black">QuadB Tech Assesment</h1>
+      <h1 className="md:text-3xl text-xl font-medium text-black">
+        QuadB Tech Assesment
+      </h1>
       <AddTodo />
       <Todos />
     </>
